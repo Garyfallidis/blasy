bc2e8ca6083779ac6ff22b05afed256b81f23966
Eleftherios Garyfallidis <garyfallidis@gmail.com>
2013-07-04 15:13:41 -0400

    New working example added

---
+++ blas_example.py
@@ -0,0 +1,19 @@
+import numpy as np
+from openblas import test_dgemm
+
+"""
+git clone https://github.com/xianyi/OpenBLAS.git
+make
+make PREFIX=./build/ install
+
+"""
+
+
+if __name__ == '__main__':
+
+    A = np.array([[0, 1], [2, 3]], dtype='f8')
+    B = A + 1
+    C = 0 * A
+    test_dgemm(A, B, C)
+    print(C)
+    print(np.dot(A, B))
